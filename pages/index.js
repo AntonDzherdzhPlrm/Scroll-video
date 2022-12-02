67995bf6a7159a7e38b817631cc86560787015e7
Anton Dzherdzh <a.dzherdzh@plarium.com>
2022-12-02 10:49:17 +0200

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import { useEffect, useRef } from "react";
-import { ScrollComponent } from "./components";
+import { ScrollComponent } from "../components";
 
 import styles from "../styles/Home.module.css";
 
