57371a21fd945a5dff73b0abde4650119cdba2b1
Anton Dzherdzh <a.dzherdzh@plarium.com>
2022-12-14 14:44:12 +0200

    test

--- components/Sequence/ImageSequence.js
+++ components/Sequence/ImageSequence.js
@@ -11,60 +11,60 @@ const ImageSequence = forwardRef(({ progress }, ref) => {
     if (newImages[index][1] === "loading") {
       return <Skeleton width="100%" height="100%" />;
     } else {
-      // return (
-      //   <>
-      //     {/* <div style={{ display: "none" }}>
-      //       {newImages[index + 1] && newImages[index + 1][0] ? (
-      //         <Image
-      //           alt="Picture"
-      //           objectFit="cover"
-      //           priority={true}
-      //           layout="fill"
-      //           display="none"
-      //           src={newImages[index + 1][0].src}
-      //         />
-      //       ) : null}
-      //     </div> */}
-      //     <Image
-      //       alt="Picture"
-      //       objectFit="cover"
-      //       priority={true}
-      //       layout="fill"
-      //       src={newImages[index][0].src}
-      //     />
-      //   </>
-      // );
-
-      return newImages.map(
-        (item, i) =>
-          // i === index && (
-          //   <Image
-          //     ref={ref}
-          //     key={i}
-          //     // loader={myLoader}
-          //     alt="Picture of the author"
-          //     objectFit="cover"
-          //     // objectPosition="center"
-          //     priority={true}
-          //     width="1920"
-          //     height="1080"
-          //     src={item[0].src}
-          //     // {...item[0]}
-          //   />
-          // )
-        <span
-          ref={ref}
-          key={i}
-          style={{
-            display: i !== index ? "none" : "block",
-            height: "100%",
-            width: "100%",
-            backgroundImage: `url('${item[0] ? item[0].src : null}')`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
+      return (
+        <>
+          <div style={{ display: "none" }}>
+            {newImages[index + 1] && newImages[index + 1][0] ? (
+              <Image
+                alt="Picture"
+                objectFit="cover"
+                priority={true}
+                layout="fill"
+                display="none"
+                src={newImages[index + 1][0].src}
+              />
+            ) : null}
+          </div>
+          <Image
+            alt="Picture"
+            objectFit="cover"
+            priority={true}
+            layout="fill"
+            src={newImages[index][0].src}
+          />
+        </>
       );
+
+      // return newImages.map(
+      //   (item, i) =>
+      //     // i === index && (
+      //     //   <Image
+      //     //     ref={ref}
+      //     //     key={i}
+      //     //     // loader={myLoader}
+      //     //     alt="Picture of the author"
+      //     //     objectFit="cover"
+      //     //     // objectPosition="center"
+      //     //     priority={true}
+      //     //     width="1920"
+      //     //     height="1080"
+      //     //     src={item[0].src}
+      //     //     // {...item[0]}
+      //     //   />
+      //     // )
+      //   <span
+      //     ref={ref}
+      //     key={i}
+      //     style={{
+      //       display: i !== index ? "none" : "block",
+      //       height: "100%",
+      //       width: "100%",
+      //       backgroundImage: `url('${item[0] ? item[0].src : null}')`,
+      //       backgroundSize: "cover",
+      //       backgroundPosition: "center",
+      //     }}
+      //   />
+      // );
     }
   }
 });
