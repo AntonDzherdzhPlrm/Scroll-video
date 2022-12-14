bde2c60fdbd3fdd8b8cf49d9fd08a494a79cff67
Anton Dzherdzh <a.dzherdzh@plarium.com>
2022-12-14 13:50:31 +0200

    test sequence

--- components/Sequence/ImageSequence.js
+++ components/Sequence/ImageSequence.js
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 import { forwardRef } from "react";
 import { Skeleton } from "./Skeleton";
 import ImageArray from "./FrogArray";
@@ -11,30 +11,59 @@ const ImageSequence = forwardRef(({ progress }, ref) => {
     if (newImages[index][1] === "loading") {
       return <Skeleton width="100%" height="100%" />;
     } else {
-      return newImages.map((item, i) => (
-        // <Image
-        //   ref={ref}
-        //   key={i}
-        //   // loader={myLoader}
-        //   alt="Picture of the author"
-        //   // objectFit="cover"
-        //   // objectPosition="center"
-        //   priority={true}
-        //   {...item[0]}
-        // />
-        <span
-          ref={ref}
-          key={i}
-          style={{
-            display: i !== index ? "none" : "block",
-            height: "100%",
-            width: "100%",
-            backgroundImage: `url('${item[0] ? item[0].src : null}')`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-      ));
+      return (
+        <>
+          <div style={{ display: "none" }}>
+            {newImages[index + 1] && newImages[index + 1][0] ? (
+              <Image
+                alt="Picture"
+                objectFit="cover"
+                priority={true}
+                layout="fill"
+                display="none"
+                src={newImages[index + 1][0].src}
+              />
+            ) : null}
+          </div>
+          <Image
+            alt="Picture"
+            objectFit="cover"
+            priority={true}
+            layout="fill"
+            src={newImages[index][0].src}
+          />
+        </>
+      );
+      // return newImages.map(
+      //   (item, i) =>
+      //     // i === index && (
+      //     //   <Image
+      //     //     ref={ref}
+      //     //     key={i}
+      //     //     // loader={myLoader}
+      //     //     alt="Picture of the author"
+      //     //     objectFit="cover"
+      //     //     // objectPosition="center"
+      //     //     priority={true}
+      //     //     width="1920"
+      //     //     height="1080"
+      //     //     src={item[0].src}
+      //     //     // {...item[0]}
+      //     //   />
+      //     // )
+      //   <span
+      //     ref={ref}
+      //     key={i}
+      //     style={{
+      //       display: i !== index ? "none" : "block",
+      //       height: "100%",
+      //       width: "100%",
+      //       backgroundImage: `url('${item[0] ? item[0].src : null}')`,
+      //       backgroundSize: "cover",
+      //       backgroundPosition: "center",
+      //     }}
+      //   />
+      // );
     }
   }
 });
